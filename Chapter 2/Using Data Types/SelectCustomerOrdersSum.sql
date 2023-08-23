48184f0ee0e2146debd5439fa57cd5252a2d0a65
Asif Kamal <38701156+asif-kamal@users.noreply.github.com>
2023-08-23 23:02:29 +0000

    Deleted Email and added TotalDue

--- Chapter 2/Using Data Types/SelectCustomerOrdersSum.sql
+++ Chapter 2/Using Data Types/SelectCustomerOrdersSum.sql
@@ -1,7 +1,7 @@
 SELECT Customer.CustomerID,
   FirstName,
   LastName,
-  Sum(Email)
+  Sum(TotalDue)
 From Customer
   JOIN Orders on Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
